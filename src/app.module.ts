d34a62d205045c479a16f0b9e92172f770618096
cortesantonio <antonio.cortes.sotelo@gmail.com>
2025-07-18 01:20:07 -0400

    feat: add OpenAiModule to the main application module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UserModule } from './user/user.module';
 import { GroupMemberModule } from './group-member/group-member.module';
 import { AuthModule } from './auth/auth.module';
 import { QuizzesModule } from './quizzes/quizzes.module';
+import { OpenAiModule } from './open-ai/open-ai.module';
 
 @Module({
-  imports: [StudyGroupModule, UserModule, GroupMemberModule, AuthModule, QuizzesModule],
+  imports: [StudyGroupModule, UserModule, GroupMemberModule, AuthModule, QuizzesModule, OpenAiModule],
   controllers: [AppController],
   providers: [AppService],
 })
