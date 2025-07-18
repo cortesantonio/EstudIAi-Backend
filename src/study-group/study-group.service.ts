0bd213d29377b4ff9f6c8e40955bd8cd6206834e
cortesantonio <antonio.cortes.sotelo@gmail.com>
2025-07-18 01:20:34 -0400

    refactor: remove UnauthorizedException import from StudyGroupService

--- src/study-group/study-group.service.ts
+++ src/study-group/study-group.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable} from '@nestjs/common';
 import { StudyGroup } from '@prisma/client';
 import { PrismaClient } from '@prisma/client';
 
