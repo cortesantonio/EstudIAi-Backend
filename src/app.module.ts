bda2873d473e2a957784c419322b4194fd15ed04
cortesantonio <antonio.cortes.sotelo@gmail.com>
2025-07-22 23:32:46 -0400

    feat: integrate Flashcards module and service into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { GroupMemberModule } from './group-member/group-member.module';
 import { AuthModule } from './auth/auth.module';
 import { QuizzesModule } from './quizzes/quizzes.module';
 import { OpenAiModule } from './open-ai/open-ai.module';
+import { FlashcardsService } from './flashcards/flashcards.service';
+import { FlashcardsModule } from './flashcards/flashcards.module';
 
 @Module({
-  imports: [StudyGroupModule, UserModule, GroupMemberModule, AuthModule, QuizzesModule, OpenAiModule],
+  imports: [StudyGroupModule, UserModule, GroupMemberModule, AuthModule, QuizzesModule, OpenAiModule, FlashcardsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FlashcardsService],
 })
 export class AppModule {}
