e71f5dff7cf2ca73a518757784bfb976d71af5d9
cortesantonio <antonio.cortes.sotelo@gmail.com>
2025-07-25 22:09:31 -0400

    feat: se integró obtención de extractedText para generar Quizzes

--- src/quizzes/quizzes.module.ts
+++ src/quizzes/quizzes.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { QuizzesController } from './quizzes.controller';
 import { QuizzesService } from './quizzes.service';
 import { OpenAIService } from 'src/open-ai/open-ai.service';
-import { FlashcardsService } from 'src/flashcards/flashcards.service';
+import { DocumentService } from 'src/document/document.service';
 
 @Module({
   controllers: [QuizzesController],
-  providers: [QuizzesService, OpenAIService, FlashcardsService]
+  providers: [QuizzesService, OpenAIService, DocumentService]
 })
 export class QuizzesModule {}
